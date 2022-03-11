35e5ef12247a3fccac0a88f786e55fc08b8a7a58
Kyu Lee <kwooler@gmail.com>
2022-03-11 00:05:43 -0800

    Lab519

--- Module5-Lab5.19-Arnett.java
+++ Module5-Lab5.19-Arnett.java
@@ -16,11 +16,17 @@ public class LabProgram {
                 break;
             }
         }
+        // When the input value is 99,
+        // line 16 break never occured.
+        // "idx" will be 4, not 5
+        // because idx = k was executed last when the k is 4.
+        // When k is 5 at the line 13, the loop will stop.
+        // At this time, line 14 idx = k will not be executed.
 
-        for (int i = length - 1; i >= idx; i--) {
-            numbers[i + 1] = numbers[i];
+        for (int i = length - 1; i >= idx; i--) { // Thus i = 4, i >= idx is true
+            numbers[i + 1] = numbers[i]; // numbers[5] = numbers[4], Thus, 5 20 30 35 50 50
         }
-        numbers[idx] = insert_value;
+        numbers[idx] = insert_value; // and then numbers[4] = 99, thus 5 20 30 35 99 50
 
         for (int j = 0; j <= length; j++) {
             System.out.print(numbers[j] + " ");
