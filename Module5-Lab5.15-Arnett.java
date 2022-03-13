2dfab67d91329c11383ce9b16e0b1c23b7af96ce
Kyu Lee <kwooler@gmail.com>
2022-03-12 16:31:12 -0800

    Lab515

--- Module5-Lab5.15-Arnett.java
+++ Module5-Lab5.15-Arnett.java
@@ -20,21 +20,39 @@ public class LabProgram {
 
         int[] finalAry = new int[finalAryLng];
 
-        for (int k = 0; k < finalAryLng; k++) {
-            for (int l = k; l < aryLng; l++) {
-                if (userValues[l] <= maxVal) {
-                    finalAry[k] = userValues[l];
-                    break;
-                }
-            }
+        // for (int k = 0; k < finalAryLng; k++) {
+        // for (int l = k; l < aryLng; l++) {
+        // if (userValues[l] <= maxVal) {
+        // finalAry[k] = userValues[l];
+        // break;
+        // }
+        // }
 
+        // No need to use nested for loop.
+        // Based on your code, I can suggest the following code.
+        // Check out the following code
+
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  
+        // Option 1
+        int k = 0;
+        for (int l = 0; l < aryLng; l++) {
+            if (userValues[l] <= maxVal) {
+                finalAry[k] = userValues[l];
+                k += 1; // K will not be greater than "finalAryLng".
+                // break;
+            }
         }
 
-        for (int f = 0; f < finalAryLng; f++) {
-            System.out.print(finalAry[f] + ",");
+        // Option 2
+        // No need to use an extra array. 
+        // Without an extra array, "finalAry",
+        for (int f = 0; f < aryLng; f++) {
+            if (userValues[f] <= maxVal)
+                System.out.print(userValues[f] + ",");
+        }
+        // for (int f = 0; f < finalAryLng; f++) {
+        //     System.out.print(finalAry[f] + ",");
         }
         System.out.println();
 
-        
     }
-}
