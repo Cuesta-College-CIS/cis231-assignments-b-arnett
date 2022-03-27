5e560377764dcf2d2c6a146e373adad11dfb1154
Kyu Lee <kwooler@gmail.com>
2022-03-26 17:31:40 -0700

    523

---
+++ Lab523.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Lab523 {
+	public static void main(String[] args) {
+		int flag;
+		final int N = 20;
+		String[] words = new String[N];
+
+		Scanner scnr = new Scanner(System.in);
+		String key = scnr.next();
+		int cnt = scnr.nextInt();
+
+		for (int i = 0; i < cnt; i++) {
+			words[i] = scnr.next();
+		}
+		flag = 0;
+		for (int i = 0; i < cnt; i++) {
+			if (key.equals(words[i])) {
+				flag = 1;
+				break;
+			}
+		}
+		if (flag == 1)
+			System.out.println(true);
+		else
+			System.out.println(false);
+
+	}
+}
