31eb3f7537099e46f49a4702b9ba61a681402198
b-arnett <97990898+b-arnett@users.noreply.github.com>
2022-05-08 13:05:18 -0700

    Add files via upload

--- Quiz4-Arnett.java
+++ Quiz4-Arnett.java
@@ -2,37 +2,55 @@ import java.util.Scanner;
 import java.util.Arrays;
 import java.util.ArrayList;
 
-nteger sid;String sname;ArrayList<Course>clist;
+class Student {
+    Integer sid;
+    String sname;
+    ArrayList<Course> clist;
 
-t id,String name,ArrayList<Course>c){
+    public Student(int id, String name, ArrayList<Course> c) {
+        sid = id;
+        sname = name;
+        clist = c;
 
-me;clist=c;
+    }
 
-}
+    public Integer getTotalCredits() { // returns total credits of the courses
+        Integer cred = 0;
+        for (int i = 0; i < clist.size(); i++)
+            cred += ((clist.get(i)).getCredit());
 
-public I nteger getTotalCredits(){ // reurn s total credits of the courses
-Integer cred=0;for(int i=0;i<clist.size();i++)cred+=((clist.get(i)).getCredit());
+        return cred;
+    }
 
-return cred;}
+    public String toString() { // override the toString to pring the object instance,
+                               // return the string to print all info of the student object
+        String str = "Student: " + Integer.toString(sid) + " " + sname + "\n\t";
+        for (int o = 0; o < clist.size(); o++) {
+            str += (clist.get(o).toString() + "\n\t");
+        }
+        str += "\n" + "Total Credits " + Integer.toString(getTotalCredits()) + "\n";
+        return str;
 
-public String toString(){ // override the toString to pring the object instance,
-                                                                                                                        // return the string to print all info of the student object
-String str="Student: "+Integer.toString(sid)+" "+sname+"\n\t";for(int o=0;o<clist.size();o++){str+=(clist.get(o).toString()+"\n\t");}str+="\n"+"Tota l Credits "+Integer.toString(getTotalCredits())+"\n";return str;
+    }
 
-}
-
+    public Integer getId() {
+        return sid;
+    }
 }
 
 //////////////////////////////
 class Course {
     String cid;
     String cname;
+    Integer credit;
+    String grade;
 
-    ng grade;
-
-    public Course(String id, Stri
-
+    public Course(String id, String name, Integer c, String g) {
+        cid = id;
         cname = name;
+        credit = c;
+        grade = g;
+    }
 
     public Integer getCredit() {
         return credit;
@@ -46,7 +64,7 @@ class Course {
 }
 
 ///////////////////////
-public class Quiz4 { // CHANGE TO QUIZ4
+public class Main { // CHANGE TO QUIZ4
 
     public static ArrayList<Student> fillupStudent() {
         ArrayList<Student> slist = new ArrayList<Student>();
